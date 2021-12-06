85d7893fd2f1bc1e2c1639394f4cd6938625d3a8
fiorellachilcon <fiorellachilc_20@hotmail.com>
2021-12-05 20:15:03 -0500

    react small projects

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome, here you will find small react projects!</h1>
+      <span>😜</span>
+    </div>
+  );
+}
